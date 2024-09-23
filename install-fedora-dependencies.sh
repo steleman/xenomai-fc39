b1cdce88c7deced58801c6b503d5a0f25ca95f24
Stefan Teleman <stefan.teleman@gmail.com>
2024-09-23 14:06:46 -0400

    Update installation instructions.

--- install-fedora-dependencies.sh
+++ install-fedora-dependencies.sh
@@ -3,3 +3,8 @@
 echo "sudo dnf install -y audit-libs-devel fuse-devel libbabeltrace-devel libbpf-devel libcap-devel libcap-ng-devel libmnl-devel libnl3-devel newt-devel pciutils-devel xmlto glibc-static perl-generators"
 sudo dnf install -y audit-libs-devel fuse-devel libbabeltrace-devel libbpf-devel libcap-devel libcap-ng-devel libmnl-devel libnl3-devel newt-devel pciutils-devel xmlto glibc-static perl-generators
 
+echo "sudo dnf install -y bpftool gcc-plugin-devel pesign"
+sudo dnf install -y bpftool gcc-plugin-devel pesign
+
+echo "sudo dnf localinstall ./kernel-headers-uapi-6.10.10-100.fc37.x86_64.rpm"
+sudo dnf ./localinstall kernel-headers-uapi-6.10.10-100.fc37.x86_64.rpm
---
+++ install-xenomai-kernel.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ] ; then
+  echo "You must be root to do this."
+  exit 1
+fi
+
+for file in \
+  ./kernel-6.10.10-300.xenomai.fc39.src.rpm \
+  ./kernel-core-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-core-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-extra-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-internal-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-ipaclones-internal-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-tools-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-tools-libs-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-tools-libs-devel-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-devel-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-devel-matched-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-uki-virt-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-headers-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./bpftool-7.3.0-300.xenomai.fc39.x86_64.rpm \
+  ./perf-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./python3-perf-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./rtla-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./rv-6.10.10-300.xenomai.fc39.x86_64.rpm
+do
+  if [ ! -f ${file} ] ; then
+    echo "RPM ${file} does not exist."
+    exit 1
+  fi
+done
+
+dnf localinstall \
+  ./kernel-core-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-core-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-extra-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-modules-internal-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-ipaclones-internal-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-tools-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-tools-libs-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-tools-libs-devel-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-devel-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-devel-matched-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-uki-virt-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./kernel-headers-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./bpftool-7.3.0-300.xenomai.fc39.x86_64.rpm \
+  ./perf-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./python3-perf-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./rtla-6.10.10-300.xenomai.fc39.x86_64.rpm \
+  ./rv-6.10.10-300.xenomai.fc39.x86_64.rpm
+
+rpm -ivh ./kernel-6.10.10-300.xenomai.fc39.src.rpm
+echo "Done."
+
